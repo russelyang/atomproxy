d89aab3f634e72a18f88612e3c5655edd5d42c4e
RusselYang <russelyang@gmail.com>
2016-10-22 22:33:19 -0700

    prepare multipe results

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-
 var fetch = require('node-fetch');
 
 if (!String.prototype.format) {
@@ -20,8 +19,6 @@ function toJson(xml) {
 	return new Promise(function(resolve, rejecct) {
 		var parseString = require('xml2js').parseString; 
 		parseString(xml, function(err, result) {
-			console.log(err);
-			console.log(result);
 			if (!err) {
 				resolve(result);
 			} else {
@@ -40,27 +37,60 @@ function toXml(json) {
 	});	
 }
 
-var datapoint = ["https://integration.api4.origin.com"];
-
-app.get('/atom/users/:userId/commonGames', function (req, res) {
-	var userId = req.params.userId;
-	var friendIds = req.query.friendIds.split(",");
-	var authToken = req.get("authToken");
-
-	//console.log(friendIds);
-
-	fetch("{0}/atom/users/{1}/commonGames?friendIds={2}".format( 
-		datapoint[0], 
+function buildFetchPromise(datapoint, userId, friendIds, authToken) {
+	return fetch("{0}/atom/users/{1}/commonGames?friendIds={2}".format( 
+		datapoint, 
 		userId, 
 		friendIds.join(",")), 
 		{
 			headers: {
 				"authToken" : authToken
 			}
-		})
-	.then(function(body) {
+		}).then(function(body) {
 		return body.text();
-	}).then( xml => toJson(xml)).then(json => toXml(json)).then(xml => res.send(xml));
+	}).then( xml => toJson(xml));
+}
+
+var datapoint = ["https://integration.api1.origin.com",
+				 "https://integration.api2.origin.com",
+				 "https://integration.api3.origin.com",
+				 "https://integration.api4.origin.com"];
+
+app.get('/atom/users/:userId/commonGames', function (req, res) {
+	var userId = req.params.userId;
+	var friendIds = req.query.friendIds.split(",");
+	var authToken = req.get("authToken");
+
+	var promises = [],
+		batch = 0;
+		fids = [];
+
+	for(var i=0; i< friendIds.length; i++) {
+		if (fids.length < 5) {
+			fids.push(friendIds[i]);
+		} 
+		if (fids.length == 5) {
+			var fp = buildFetchPromise(datapoint[batch], userId, fids, authToken);
+			promises.push(fp);
+			batch++;
+			if (batch == 3) {
+				batch = 0;
+			}
+			fids = [];
+		}
+	}
+
+	//console.log(promises);
+
+	Promise.all(promises).then(function(jsons) {
+		console.log(jsons);
+	});
+	//console.log(friendIds);
+	///Promise.all(promises).then(all => console.log(all));
+	
+	buildFetchPromise(datapoint[0], userId, friendIds, authToken)
+		.then(json => toXml(json)).then(xml => res.send(xml));
+
 
   	//res.send('Hello World!' + userId + 'friendIds: ' + friendIds + "authToken: " + authToken );
 });
